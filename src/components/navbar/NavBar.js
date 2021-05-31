589c4d5252ef49df233e4c07c3c284db189facee
Andres Fernandez <andresfernandez.mdp@gmail.com>
2021-05-31 12:39:47 -0300

    Navbar, font-family

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,48 @@
+import '../main/Main.css';
+
+function NavBar() {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light fw-bold bg-site-main">
+			<div className="container-fluid">
+				<a className="navbar-brand h1 fs-2" href="#">
+					Lost Pets
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav">
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div
+					className="collapse navbar-collapse d-flex-md justify-content-end"
+					id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Perdidos
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Encontrados
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Que Hacemos?
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Contacto
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export default NavBar;
