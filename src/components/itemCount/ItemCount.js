19defc1b6e8dcc64f1f41a62935ae9bce18cb8ed
Andres Fernandez <andresfernandez.mdp@gmail.com>
2021-06-09 15:38:45 -0300

    itemCount improve

--- src/components/itemCount/ItemCount.js
+++ src/components/itemCount/ItemCount.js
@@ -3,30 +3,33 @@ import React, { useState, useEffect } from 'react';
 const ItemCount = ({ initial, stock, onAdd }) => {
 	const [items, setItems] = useState(initial);
 
+	useEffect((items)=>{
+		console.log(`Se seleccionaron: ${items}`);
+	},[items]);
+
 	return (
 		<>
 			<div className="my-3">
-				<button
-					className="btn btn-primary me-2"
-					onClick={() => {
-						items > 1 && setItems(items - 1);
-					}}>
+				<button id="dashCount" className="btn me-4"> 
 					<i
-						className="bi bi-dash-square-fill"
-						style={{ fontSize: '1rem', color: 'white' }}
+						className="bi bi-dash-square"
+						style={{ fontSize: '2rem' }}
+						onClick={() => {
+							items > 1 && setItems(items - 1);
+						}}
 					/>
-				</button>
+				</button >
 				<span className="fs-3 bold align-middle">{items}</span>
-				<button
-					className="btn btn-primary ms-2"
-					onClick={() => {
-						items < stock
-							? setItems(items + 1)
-							: alert('Sin disponibilidad');
-					}}>
+				<button id="addCount" className="btn ms-4">
 					<i
-						className="bi bi-plus-square-fill"
-						style={{ fontSize: '1rem', color: 'white' }}
+						className="bi bi-plus-square"
+						style={{ fontSize: '2rem'}}
+						onClick={() => {
+							items < stock
+							? setItems(items + 1) 
+							//: document.getElementById("addCount").attributes.disabled;
+							: alert("Sin disponibilidad");
+						}}
 					/>
 				</button>
 			</div>
