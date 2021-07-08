fef76835a4c3a7fb17ccdba4f3bf7bc5461dcc99
Andres Fernandez <andresfernandez.mdp@gmail.com>
2021-07-07 23:55:29 -0300

    start CartWidget conditional

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,42 +1,73 @@
 import { useContext } from 'react';
 import CartContext from '../../context/CartContext';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
 	const { items, removeItem, clear } = useContext(CartContext);
 
 	return (
 		<>
-			<h3>Detalle de Compra</h3>
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th scope="col">Producto</th>
-						<th scope="col">Cantidad</th>
-						<th scope="col">Total</th>
-					</tr>
-				</thead>
-				{items.map((item) => (
-					<tbody>
-						<tr>
-							<th scope="row">{item.productDetail.title}</th>
-							<td>{item.quantity}</td>
-							<td>{item.productDetail.price}</td>
-
-							<button
-								className="btn bi bi-x-square-fill"
-								onClick={() =>
-									removeItem(item.productDetail.id)
-								}></button>
-						</tr>
-					</tbody>
-				))}
-			</table>
-			<button onClick={() => clear()}>
-				<i className="bi bi-trash" /> Eliminar Todo
-			</button>
-			<button>
-				<i className="bi bi-arrow-left-square"> Seguir Comprando</i>
-			</button>
+			{items.length === 0 ? (
+				<div className="text-center">
+					<h3>No hay productos en el Carrito</h3>
+					<Link
+						className="btn btn-primary mt-4 fw-bold"
+						exact
+						to={`/`}>
+						<i className="bi bi-arrow-left-square" /> Ir a la Tienda
+					</Link>
+				</div>
+			) : (
+				<div>
+					<h3>Detalle de Compra</h3>
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th scope="col">Producto</th>
+								<th scope="col">Cantidad</th>
+								<th scope="col">Total</th>
+							</tr>
+						</thead>
+						{items.map((item) => (
+							<tbody>
+								<tr>
+									<th scope="row">
+										{item.productDetail.title}
+									</th>
+									<td>{item.quantity}</td>
+									<td>{item.productDetail.price}</td>
+									<td
+										className="btn w-auto bg-danger ms-1"
+										onClick={() =>
+											removeItem(item.productDetail.id)
+										}>
+										<i
+											className="bi bi-dash-lg"
+											style={{
+												fontSize: '1.1rem'
+											}}
+										/>
+									</td>
+								</tr>
+							</tbody>
+						))}
+					</table>
+					<div className="text-center">
+						<button
+							className="btn btn-danger me-2 fw-bold"
+							onClick={() => clear()}>
+							<i className="bi bi-trash" /> Eliminar Todo
+						</button>
+						<Link
+							className="btn btn-primary fw-bold"
+							exact
+							to={`/`}>
+							<i className="bi bi-arrow-left-square" /> Seguir
+							Comprando
+						</Link>
+					</div>
+				</div>
+			)}
 		</>
 	);
 };
--- src/components/cartWidget/CartWidget.js
+++ src/components/cartWidget/CartWidget.js
@@ -1,11 +1,20 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import CartContext from '../../context/CartContext';
 const CartWidget = () => {
+	const { items } = useContext(CartContext);
 	return (
-		<a className="ms-3" href="#">
-			<i
-				className="bi bi-cart4"
-				style={{ fontSize: '2rem', color: 'black' }}
-			/>
-		</a>
+		<>
+			<Link exact to="/cart" className="ms-3">
+				<i
+					className="bi bi-cart4"
+					style={{ fontSize: '2rem', color: 'black' }}
+				/>
+			</Link>
+			{items.length !== 0 && (
+				<span>{items.map((item) => item.quantity)}</span>
+			)}
+		</>
 	);
 };
 export default CartWidget;
